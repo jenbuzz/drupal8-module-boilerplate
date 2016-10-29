f6df85259b781d55d74e4c7831e5b1c23748962e
dan-lyn <daniel_lynge@hotmail.com>
2016-10-29 09:57:57 +0200

    example of a custom block made setup in a module

---
+++ src/Plugin/Block/DrupalBoilerplateBlock.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace Drupal\drupal8_module_boilerplate\Plugin\Block;
+
+use Drupal\Core\Block\BlockBase;
+use Drupal\Core\Form\FormStateInterface;
+
+/**
+ * Provides a 'Drupal Boilerplate' block.
+ *
+ * @Block(
+ *   id = "drupalboilerplate_block",
+ *   admin_label = @Translation("Drupal boilerplate block"),
+ * )
+ */
+class DrupalBoilerplateBlock extends BlockBase {
+
+    /**
+     * {@inheritdoc}
+     */
+    public function build() {
+      return array(
+        '#theme' => 'drupalboilerplate-block',
+      );
+    }
+
+}
